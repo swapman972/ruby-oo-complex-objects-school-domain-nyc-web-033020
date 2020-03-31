70e8f6f3c02b4254fc5cb3ff6200d5f05eebd6a8
swapman972 <jordan.melidor@hotmail.fr>
2020-03-31 16:59:29 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+
+class School
+    def initialize (name)
+        @name = name
+        @roster = {}
+    end
+    attr_reader :name
+    attr_reader :roster
+    def add_student (name, grade)
+        if roster[grade] == nil
+            roster[grade] = []
+            roster[grade] << name
+        else 
+            roster[grade] << name
+        end
+    end
+
+    def grade(num)
+        roster[num]
+    end
+    def sort
+        roster.each do |key, value| 
+            value.sort!
+        end
+    end
+end
